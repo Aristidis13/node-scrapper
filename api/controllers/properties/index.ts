143e159b39d1d789dc02ad5d74dc250c52449fb2
Aris Barlos <aris13pat@hotmail.com>
2024-02-08 02:06:13 +0200

    Minor commits and refactors & typing

--- api/controllers/properties/index.ts
+++ api/controllers/properties/index.ts
@@ -1,6 +1,36 @@
-/**
- * Contains imports for every site we extract properties from
- */
-import getPropertiesFromXE from "./xe";
+/* eslint-env browser */
+import puppeteer from "puppeteer-extra";
+import StealthPlugin from "puppeteer-extra-plugin-stealth";
+import { siteData } from "../../../config/constants";
+import { autoScroll } from "../../controllers/common";
+import { IResults, ISearchParameters } from "../../interfaces-types/properties";
+import { makeString } from "../../helpers/common";
+import { createUrl } from "../../helpers/properties";
 
-export { getPropertiesFromXE };
+puppeteer.use(StealthPlugin()); // eslint-disable-line new-cap
+
+async function scrape(searchParams: ISearchParameters) {
+  const results: IResults = {};
+  const browser = await puppeteer.launch({ headless: true });
+
+  for (const site of siteData) {
+    const id = makeString(site.id) ?? JSON.stringify(site);
+    const page = await browser.newPage();
+    const apiUrl: string | null = createUrl(searchParams, site.id);
+
+    if (!apiUrl) {
+      continue;
+    }
+    await page.goto(apiUrl);
+
+    await page.setViewport({ width: 2400, height: 800 }); // prettier-ignore
+
+    await autoScroll(page);
+    results[id] = await page.evaluate(site?.scrape) ?? `Evaluation for ${id} failed.`; // prettier-ignore
+  }
+  await browser.close();
+
+  return results;
+}
+
+export default scrape;
